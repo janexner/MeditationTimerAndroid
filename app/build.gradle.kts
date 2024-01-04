2a79b6fdd374d00dc9a5c4dfd0ff96fb557c966b
jexner <jan.exner@gmx.net>
2024-01-04 21:32:16 +0100

    In which our hero revives another app

---
+++ app/build.gradle.kts
@@ -0,0 +1,111 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.exner.tools.meditationtimer"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.exner.tools.meditationtimer"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 2
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
+        }
+        debug {
+            isDebuggable = true
+        }
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.7"
+        // see https://developer.android.com/jetpack/androidx/releases/compose-kotlin
+    }
+    buildToolsVersion = "34.0.0"
+}
+
+kotlin {
+    jvmToolchain(17)
+
+    sourceSets.main {
+        kotlin.srcDir("build/generated/ksp/main/kotlin")
+    }
+    sourceSets.test {
+        kotlin.srcDir("build/generated/ksp/test/kotlin")
+    }
+}
+
+dependencies {
+    // now sorted by https://developer.android.com/jetpack/androidx/releases
+
+    // jetpack
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.foundation)
+    implementation(libs.androidx.foundation.layout)
+    implementation(libs.androidx.material.icons.core)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.windowSizeClass)
+    implementation(libs.androidx.runtime)
+    implementation(libs.androidx.runtime.livedata)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewModelCompose)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.common.java8)
+    implementation(libs.androidx.webkit)
+    implementation(libs.androidx.window.manager)
+    implementation(libs.androidx.preferences)
+
+    // hilt
+    implementation(libs.hilt.android)
+    ksp(libs.dagger.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    // Room components
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)
+
+    // others
+    implementation(libs.material)
+
+    // compose destinations
+    implementation(libs.compose.destinations.core)
+    ksp(libs.compose.destinations.ksp)
+
+    // testing
+    testImplementation(libs.junit)
+}
+
+hilt {
+    enableAggregatingTask = true
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/src/main/assets/schemas")
+    arg("room.incremental"   , "true")
+}
