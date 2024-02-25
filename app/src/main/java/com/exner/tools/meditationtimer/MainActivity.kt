dff310fde2908edb6c6a4b7a56584ecccabbd9ff
jexner <jan.exner@gmx.net>
2024-02-25 16:37:21 +0100

    in which our hero goes edge to edge

--- app/src/main/java/com/exner/tools/meditationtimer/MainActivity.kt
+++ app/src/main/java/com/exner/tools/meditationtimer/MainActivity.kt
@@ -5,8 +5,16 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Vibrator
 import android.os.VibratorManager
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import com.exner.tools.meditationtimer.audio.SoundPoolHolder
 import com.exner.tools.meditationtimer.audio.VibratorHolder
 import com.exner.tools.meditationtimer.ui.destinations.MeditationTimerGlobalScaffold
@@ -18,6 +26,23 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v: View, insets: WindowInsetsCompat ->
+            val systemInsets: Insets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(
+                v.paddingLeft,
+                v.paddingTop,
+                v.paddingRight,
+                v.paddingBottom - systemInsets.bottom
+            ) // subtract the insets from the bottom padding
+            insets
+        }
 
         setContent {
             MeditationTimerTheme {
