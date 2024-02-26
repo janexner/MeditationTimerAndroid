7a4c6ca1184d6e92409373c40def64b2e1a184a5
jexner <jan.exner@gmx.net>
2024-02-26 16:07:28 +0100

    in which our hero changes the About screen

--- app/src/main/java/com/exner/tools/meditationtimer/ui/destinations/About.kt
+++ app/src/main/java/com/exner/tools/meditationtimer/ui/destinations/About.kt
@@ -1,22 +1,28 @@
 package com.exner.tools.meditationtimer.ui.destinations
 
+import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import com.exner.tools.meditationtimer.BuildConfig
 import com.exner.tools.meditationtimer.ui.HeaderText
 import com.exner.tools.meditationtimer.ui.theme.MeditationTimerTheme
@@ -34,6 +40,7 @@ fun About() {
             text = "About Meditation Timer",
             modifier = Modifier.padding(8.dp)
         )
+        Spacer(modifier = Modifier.height(8.dp))
 
         // what's the orientation, right now?
         val configuration = LocalConfiguration.current
@@ -45,11 +52,7 @@ fun About() {
                         .fillMaxSize()
                         .padding(8.dp)
                 ) {
-                    Text(
-                        text = "Meditation Timer ${BuildConfig.VERSION_NAME}",
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(8.dp)
-                    )
+                    AboutVersionAndButton()
                     Spacer(modifier = Modifier.height(8.dp))
                     AboutText()
                 }
@@ -62,12 +65,7 @@ fun About() {
                         .fillMaxSize()
                         .padding(8.dp)
                 ) {
-                    Text(
-                        text = "Meditation Timer ${BuildConfig.VERSION_NAME}",
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(8.dp)
-                    )
-
+                    AboutVersionAndButton()
                     Spacer(modifier = Modifier.height(8.dp))
                     AboutText()
                 }
@@ -76,6 +74,28 @@ fun About() {
     }
 }
 
+@Composable
+private fun AboutVersionAndButton() {
+    val localContext = LocalContext.current
+    Column() {
+        Text(
+            text = "Meditation Timer ${BuildConfig.VERSION_NAME}",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Button(
+            onClick = {
+                val webpage: Uri =
+                    Uri.parse("https://jan-exner.de/software/android/meditationtimer/")
+                val intent = Intent(Intent.ACTION_VIEW, webpage)
+                startActivity(localContext, intent, null)
+            },
+        ) {
+            Text(text = "Visit the Meditation Timer web site")
+        }
+    }
+}
 @Composable
 fun AboutText() {
     Column(
