b82dd8bb912b996fd4d81263ed4735b0eff56826
jexner <jan.exner@gmx.net>
2024-06-10 23:23:38 +0200

    in which our hero rebases

--- app/src/main/java/com/exner/tools/meditationtimer/network/TimerConnectionLifecycleCallback.kt
+++ app/src/main/java/com/exner/tools/meditationtimer/network/TimerConnectionLifecycleCallback.kt
@@ -1,31 +1,16 @@
 package com.exner.tools.meditationtimer.network
 
-import android.content.Context
-import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.connection.ConnectionInfo
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback
 import com.google.android.gms.nearby.connection.ConnectionResolution
-import com.google.android.gms.nearby.connection.ConnectionsStatusCodes
 
-
-
-class TimerConnectionLifecycleCallback(
-    val context: Context
-) : ConnectionLifecycleCallback() {
+class TimerConnectionLifecycleCallback: ConnectionLifecycleCallback() {
     override fun onConnectionInitiated(p0: String, p1: ConnectionInfo) {
-        // Automatically accept the connection on both sides.
-        val payloadCallback = TimerPayloadCallback()
-        Nearby.getConnectionsClient(context).acceptConnection("com.exner.tools.activitytimerfortv", payloadCallback)
+        TODO("Not yet implemented")
     }
 
-    override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
-        when (result.status.statusCode) {
-            ConnectionsStatusCodes.STATUS_OK -> {}
-            ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {}
-            ConnectionsStatusCodes.STATUS_ERROR -> {}
-            else -> {}
-        }
-
+    override fun onConnectionResult(p0: String, p1: ConnectionResolution) {
+        TODO("Not yet implemented")
     }
 
     override fun onDisconnected(p0: String) {
