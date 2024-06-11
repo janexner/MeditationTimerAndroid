e5ff5d1ba43c8b572007cf12c7d88dd657e56e1e
jexner <jan.exner@gmx.net>
2024-06-11 21:04:17 +0200

    in which our hero rebases

--- app/src/main/java/com/exner/tools/meditationtimer/network/TimerConnectionLifecycleCallback.kt
+++ app/src/main/java/com/exner/tools/meditationtimer/network/TimerConnectionLifecycleCallback.kt
@@ -1,16 +1,31 @@
 package com.exner.tools.meditationtimer.network
 
+import android.content.Context
+import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.connection.ConnectionInfo
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback
 import com.google.android.gms.nearby.connection.ConnectionResolution
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes
 
-class TimerConnectionLifecycleCallback: ConnectionLifecycleCallback() {
+
+
+class TimerConnectionLifecycleCallback(
+    val context: Context
+) : ConnectionLifecycleCallback() {
     override fun onConnectionInitiated(p0: String, p1: ConnectionInfo) {
-        TODO("Not yet implemented")
+        // Automatically accept the connection on both sides.
+        val payloadCallback = TimerPayloadCallback()
+        Nearby.getConnectionsClient(context).acceptConnection("com.exner.tools.activitytimerfortv", payloadCallback)
     }
 
-    override fun onConnectionResult(p0: String, p1: ConnectionResolution) {
-        TODO("Not yet implemented")
+    override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+        when (result.getStatus().getStatusCode()) {
+            ConnectionsStatusCodes.STATUS_OK -> {}
+            ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {}
+            ConnectionsStatusCodes.STATUS_ERROR -> {}
+            else -> {}
+        }
+
     }
 
     override fun onDisconnected(p0: String) {
--- app/src/main/java/com/exner/tools/meditationtimer/ui/SendToNearbyDeviceViewModel.kt
+++ app/src/main/java/com/exner/tools/meditationtimer/ui/SendToNearbyDeviceViewModel.kt
@@ -1,12 +1,19 @@
 package com.exner.tools.meditationtimer.ui
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.exner.tools.meditationtimer.data.persistence.MeditationTimerDataRepository
+import com.exner.tools.meditationtimer.network.TimerEndpointDiscoveryCallback
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.DiscoveryOptions
+import com.google.android.gms.nearby.connection.Strategy
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
+
 
 enum class ProcessStateConstants {
     AWAITING_PERMISSIONS,
@@ -18,6 +25,10 @@ enum class ProcessStateConstants {
     DISCONNECTED,
     ERROR
 }
+
+const val endpointId = "com.exner.tools.activitytimerfortv"
+const val userName = "Anonymous"
+
 data class ProcessState(
     val currentState: ProcessStateConstants = ProcessStateConstants.AWAITING_PERMISSIONS
 )
@@ -25,7 +36,7 @@ data class ProcessState(
 @HiltViewModel
 class SendToNearbyDeviceViewModel @Inject constructor(
     repository: MeditationTimerDataRepository
-): ViewModel() {
+) : ViewModel() {
 
     private val _processStateFlow = MutableStateFlow(ProcessState())
     val processStateFlow: StateFlow<ProcessState> = _processStateFlow.asStateFlow()
@@ -33,4 +44,25 @@ class SendToNearbyDeviceViewModel @Inject constructor(
     fun setCurrentState(newState: ProcessStateConstants) {
         _processStateFlow.value = ProcessState(newState)
     }
+
+    fun startDiscovery(context: Context) {
+        val discoveryOptions =
+            DiscoveryOptions.Builder().setStrategy(Strategy.P2P_POINT_TO_POINT).build()
+        val endpointDiscoveryCallback = TimerEndpointDiscoveryCallback(context = context)
+        Nearby.getConnectionsClient(context)
+            .startDiscovery(
+                endpointId,
+                endpointDiscoveryCallback,
+                discoveryOptions
+            )
+            .addOnSuccessListener { unused: Void? ->
+                Log.d("SNDVM", "Success! Discovered a nearby device!")
+            }
+            .addOnFailureListener { e: Exception? ->
+                if (e != null) {
+                    Log.d("SNDVM", "Error discovering: ${e.message}")
+                }
+            }
+    }
+
 }
