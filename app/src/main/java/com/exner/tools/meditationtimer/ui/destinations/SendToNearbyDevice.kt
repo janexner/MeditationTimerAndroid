bb095aafd1b614571c2633f6e5cd368ed86cf470
jexner <jan.exner@gmx.net>
2024-06-13 17:31:38 +0200

    in which our hero proceeds slowly toward connecting nearby

--- app/src/main/java/com/exner/tools/meditationtimer/ui/destinations/SendToNearbyDevice.kt
+++ app/src/main/java/com/exner/tools/meditationtimer/ui/destinations/SendToNearbyDevice.kt
@@ -13,11 +13,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.exner.tools.meditationtimer.network.TimerEndpointDiscoveryCallback
 import com.exner.tools.meditationtimer.ui.ProcessStateConstants
 import com.exner.tools.meditationtimer.ui.SendToNearbyDeviceViewModel
 import com.exner.tools.meditationtimer.ui.tools.Permissions
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.android.gms.nearby.Nearby
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -35,14 +37,23 @@ fun SendToNearbyDevice(
 
     val processState by sendToNearbyDeviceViewModel.processStateFlow.collectAsState()
 
+    val endpointDiscoveryCallback = TimerEndpointDiscoveryCallback(context = context)
+    sendToNearbyDeviceViewModel.provideDiscoveryCallback(endpointDiscoveryCallback)
+    val connectionsClient = Nearby.getConnectionsClient(context)
+    sendToNearbyDeviceViewModel.provideConnectionsClient(connectionsClient)
+
     Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
         // some sanity checking for state
         if (processState.currentState == ProcessStateConstants.AWAITING_PERMISSIONS && permissionsNeeded.allPermissionsGranted) {
-            sendToNearbyDeviceViewModel.setCurrentState(ProcessStateConstants.AWAITING_DISCOVERY)
+            sendToNearbyDeviceViewModel.transitionToNewState(ProcessStateConstants.PERMISSIONS_GRANTED)
         }
+
+        Spacer(modifier = Modifier.size(16.dp))
+        Text(text = "Current ProcessState is ${processState.currentState.name}")
+        Spacer(modifier = Modifier.size(16.dp))
 
         // UI, depending on state
         when (processState.currentState) {
@@ -56,6 +67,20 @@ fun SendToNearbyDevice(
                         }
                     ) {
                         Text(text = "Request permissions")
+                    }
+                }
+            }
+
+            ProcessStateConstants.PERMISSIONS_GRANTED -> {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Text(text = "All permissions OK.")
+                    Spacer(modifier = Modifier.size(16.dp))
+                    Button(
+                        onClick = {
+                            sendToNearbyDeviceViewModel.transitionToNewState(ProcessStateConstants.DISCOVERY_STARTED)
+                        }
+                    ) {
+                        Text(text = "Start discovery")
                     }
                 }
             }
@@ -74,22 +99,39 @@ fun SendToNearbyDevice(
                 }
             }
 
-            ProcessStateConstants.AWAITING_DISCOVERY -> {
+            ProcessStateConstants.DISCOVERY_STARTED -> {
                 Column(modifier = Modifier.fillMaxSize()) {
                     Text(text = "All permissions OK, looking for devices...")
-                    sendToNearbyDeviceViewModel.startDiscovery(context = context)
                 }
             }
 
-            ProcessStateConstants.DISCOVERED -> TODO()
-            ProcessStateConstants.CONNECTED -> TODO()
-            ProcessStateConstants.RECEIVING -> TODO()
+            ProcessStateConstants.FOUND_PARTNER -> {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Text(text = "Found a partner!")
+                }
+            }
+            ProcessStateConstants.AUTHENTICATION_OK -> TODO()
+            ProcessStateConstants.AUTHENTICATION_DENIED -> TODO()
+            ProcessStateConstants.CONNECTION_ESTABLISHED -> TODO()
+            ProcessStateConstants.CONNECTION_DENIED -> TODO()
+            ProcessStateConstants.SENDING -> TODO()
             ProcessStateConstants.DISCONNECTED -> TODO()
+            ProcessStateConstants.DONE -> {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Text(text = "All done.")
+                }
+            }
+            ProcessStateConstants.CANCELLED -> {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Text(text = "Cancelled.")
+                }
+            }
             ProcessStateConstants.ERROR -> {
                 Column(modifier = Modifier.fillMaxSize()) {
                     Text(text = "Some error occurred. It may help to move away from this screen and try it all again.")
                 }
             }
+
         }
     }
 }
