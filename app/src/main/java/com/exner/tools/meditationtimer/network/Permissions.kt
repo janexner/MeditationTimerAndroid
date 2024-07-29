25d3c656ba3fe1143092f26b0f349b4d80a447d5
jexner <jan.exner@gmx.net>
2024-07-29 11:16:33 +0200

    in which our hero rebases

--- app/src/main/java/com/exner/tools/meditationtimer/network/Permissions.kt
+++ app/src/main/java/com/exner/tools/meditationtimer/network/Permissions.kt
@@ -1,103 +1,77 @@
 package com.exner.tools.meditationtimer.network
 
+import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
-import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
-
-private const val MINSDKVERSION = 28
 
 class Permissions(
     val context: Context
 ) {
-    data class IndividualPermission(
-        val name: String,
-        val minSdkVersion: Int,
-        val maxSdkVersion: Int? = null
-    )
-
-    private val allNecessaryPermissions = listOf(
-        IndividualPermission(
-            name = "android.permission.ACCESS_WIFI_STATE",
-            minSdkVersion = MINSDKVERSION,
-            maxSdkVersion = 35
-        ),
-        IndividualPermission(
-            name = "android.permission.CHANGE_WIFI_STATE",
-            minSdkVersion = MINSDKVERSION,
-            maxSdkVersion = 35
-        ),
-        IndividualPermission(
-            name = "android.permission.BLUETOOTH",
-            minSdkVersion = MINSDKVERSION,
-            maxSdkVersion = 30
-        ),
-        IndividualPermission(
-            name = "android.permission.BLUETOOTH_ADMIN",
-            minSdkVersion = MINSDKVERSION,
-            maxSdkVersion = 30
-        ),
-        IndividualPermission(
-            name = "android.permission.ACCESS_COARSE_LOCATION",
-            minSdkVersion = MINSDKVERSION,
-            maxSdkVersion = 28
-        ),
-        IndividualPermission(
-            name = "android.permission.ACCESS_FINE_LOCATION",
-            minSdkVersion = 29,
-            maxSdkVersion = 31
-        ),
-        IndividualPermission(
-            name = "android.permission.BLUETOOTH_ADVERTISE",
-            minSdkVersion = 31,
-        ),
-        IndividualPermission(
-            name = "android.permission.BLUETOOTH_CONNECT",
-            minSdkVersion = 31,
-        ),
-        IndividualPermission(
-            name = "android.permission.BLUETOOTH_SCAN",
-            minSdkVersion = 31,
-        ),
-        IndividualPermission(
-            name = "android.permission.NEARBY_WIFI_DEVICES",
-            minSdkVersion = 32,
-        ),
-    )
 
     private val currentReleaseLevel: Int = Build.VERSION.SDK_INT
 
-    private val missingPermissions: MutableList<IndividualPermission> = mutableListOf()
+    val allNecessaryPermissions = when (currentReleaseLevel) {
 
-    private val allNecessaryPermissionsAsListOfStrings: MutableList<String> = mutableListOf()
+        Build.VERSION_CODES.TIRAMISU, Build.VERSION_CODES.UPSIDE_DOWN_CAKE -> {
+            listOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_ADVERTISE,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.NEARBY_WIFI_DEVICES,
+            )
+        }
 
-    // check all the permissions we need
-    init {
-        allNecessaryPermissions.forEach { individualPermission ->
-            if (individualPermission.minSdkVersion <= currentReleaseLevel && (individualPermission.maxSdkVersion == null || individualPermission.maxSdkVersion >= currentReleaseLevel)) {
-                allNecessaryPermissionsAsListOfStrings.add(individualPermission.name)
-                val checkResult =
-                    ContextCompat.checkSelfPermission(context, individualPermission.name)
-                Log.d("PERMISSIONS", "Permission ${individualPermission.name} is $checkResult")
-                if (checkResult == PackageManager.PERMISSION_DENIED) {
-                    missingPermissions.add(individualPermission)
-                } else if (checkResult == PackageManager.PERMISSION_GRANTED) {
-                    missingPermissions.remove(individualPermission)
-                }
-            }
+        Build.VERSION_CODES.S_V2, Build.VERSION_CODES.S -> {
+            listOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_ADVERTISE,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        }
+
+        Build.VERSION_CODES.R, Build.VERSION_CODES.Q -> {
+            listOf(
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        }
+
+        else -> {
+            listOf(
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
         }
     }
 
-    fun getAllNecessaryPermissionsAsListOfStrings(): List<String> {
-        return allNecessaryPermissionsAsListOfStrings
-    }
+    private val missingPermissions: MutableList<String> = mutableListOf()
 
-    fun doWeHaveAllPermissions(): Boolean {
-        return (0 == missingPermissions.size)
-    }
-
-    fun getMissingPermissions(): List<IndividualPermission> {
-        return missingPermissions
+    // check all the permissions we need
+    init {
+        allNecessaryPermissions.forEach { permission ->
+            val checkResult =
+                ContextCompat.checkSelfPermission(context, permission)
+            if (checkResult == PackageManager.PERMISSION_DENIED) {
+                missingPermissions.add(permission)
+            } else if (checkResult == PackageManager.PERMISSION_GRANTED) {
+                missingPermissions.remove(permission)
+            }
+        }
     }
 }
--- app/src/main/java/com/exner/tools/meditationtimer/ui/destinations/SendToNearbyDevice.kt
+++ app/src/main/java/com/exner/tools/meditationtimer/ui/destinations/SendToNearbyDevice.kt
@@ -1,25 +1,39 @@
 package com.exner.tools.meditationtimer.ui.destinations
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.exner.tools.meditationtimer.network.TimerEndpointDiscoveryCallback
+import com.exner.tools.meditationtimer.network.Permissions
+import com.exner.tools.meditationtimer.ui.ProcessState
 import com.exner.tools.meditationtimer.ui.ProcessStateConstants
 import com.exner.tools.meditationtimer.ui.SendToNearbyDeviceViewModel
-import com.exner.tools.meditationtimer.ui.tools.Permissions
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import com.google.android.gms.nearby.Nearby
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -32,106 +46,245 @@ fun SendToNearbyDevice(
 ) {
     val context = LocalContext.current
     val permissions = Permissions(context = context)
+
     val permissionsNeeded =
-        rememberMultiplePermissionsState(permissions = permissions.getAllNecessaryPermissionsAsListOfStrings())
+        rememberMultiplePermissionsState(
+            permissions = permissions.allNecessaryPermissions,
+            onPermissionsResult = { resultsMap ->
+                resultsMap.forEach{ result ->
+                    Log.d("STND", "${result.key} : ${result.value}")
+                }
+            }
+        )
 
     val processState by sendToNearbyDeviceViewModel.processStateFlow.collectAsState()
 
-    val endpointDiscoveryCallback = TimerEndpointDiscoveryCallback(context = context)
-    sendToNearbyDeviceViewModel.provideDiscoveryCallback(endpointDiscoveryCallback)
-    val connectionsClient = Nearby.getConnectionsClient(context)
-    sendToNearbyDeviceViewModel.provideConnectionsClient(connectionsClient)
+    // some sanity checking for state
+    Log.d("STND", "All permissions granted: ${permissionsNeeded.allPermissionsGranted}")
+    if (processState.currentState == ProcessStateConstants.AWAITING_PERMISSIONS && permissionsNeeded.allPermissionsGranted) {
+        sendToNearbyDeviceViewModel.transitionToNewState(ProcessStateConstants.PERMISSIONS_GRANTED)
+    } else if (processState.currentState == ProcessStateConstants.AWAITING_PERMISSIONS) {
+        Log.d("STND", "Missing permissions: ${permissionsNeeded.permissions}")
+    }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        // some sanity checking for state
-        if (processState.currentState == ProcessStateConstants.AWAITING_PERMISSIONS && permissionsNeeded.allPermissionsGranted) {
-            sendToNearbyDeviceViewModel.transitionToNewState(ProcessStateConstants.PERMISSIONS_GRANTED)
+    Scaffold(
+        content = { innerPadding ->
+            val configuration = LocalConfiguration.current
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .padding(8.dp)
+                    .fillMaxSize()
+            ) {
+                Spacer(modifier = Modifier.size(16.dp))
+                Text(text = "Now ${processState.currentState.name}")
+                Spacer(modifier = Modifier.size(16.dp))
+
+                // UI, depending on state
+                when (processState.currentState) {
+                    ProcessStateConstants.AWAITING_PERMISSIONS -> {
+                        ProcessStateAwaitingPermissionsScreen(permissionsNeeded)
+                    }
+
+                    ProcessStateConstants.PERMISSIONS_GRANTED -> {
+                        ProcessStatePermissionsGrantedScreen()
+                    }
+
+                    ProcessStateConstants.PERMISSIONS_DENIED -> {
+                        ProcessStateAwaitingPermissionsScreen(permissionsNeeded)
+                    }
+
+                    ProcessStateConstants.DISCOVERY_STARTED -> {
+                        ProcessStateDiscoveryStartedScreen()
+                    }
+
+                    ProcessStateConstants.PARTNER_FOUND -> {
+                        ProcessStatePartnerFoundScreen()
+                    }
+
+                    ProcessStateConstants.CONNECTING -> {
+                        ProcessStateConnectingScreen()
+                    }
+
+                    ProcessStateConstants.AUTHENTICATION_OK -> {}
+                    ProcessStateConstants.AUTHENTICATION_DENIED -> {}
+                    ProcessStateConstants.CONNECTION_ESTABLISHED -> {}
+                    ProcessStateConstants.CONNECTION_DENIED -> {}
+                    ProcessStateConstants.SENDING -> {}
+                    ProcessStateConstants.DISCONNECTED -> {}
+                    ProcessStateConstants.DONE -> {
+                        ProcessStateDoneScreen()
+                    }
+
+                    ProcessStateConstants.CANCELLED -> {
+                        ProcessStateCancelledScreen()
+                    }
+
+                    ProcessStateConstants.ERROR -> {
+                        ProcessStateErrorScreen()
+                    }
+
+                }
+            }
+        },
+        bottomBar = {
+            SendToNearbyBottomBar(
+                processState = processState,
+                transition = sendToNearbyDeviceViewModel::transitionToNewState
+            )
         }
+    )
+}
 
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun ProcessStateAwaitingPermissionsScreen(permissionsNeeded: MultiplePermissionsState) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "If you would like to send processes to your TV, this app needs permission for Bluetooth, WiFi, and the discovery of nearby devices, which may also need location permissions.")
         Spacer(modifier = Modifier.size(16.dp))
-        Text(text = "Current ProcessState is ${processState.currentState.name}")
-        Spacer(modifier = Modifier.size(16.dp))
-
-        // UI, depending on state
-        when (processState.currentState) {
-            ProcessStateConstants.AWAITING_PERMISSIONS -> {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "If you would like to send processes to your TV, this app needs permission for Bluetooth, WiFi, and the discovery of nearby devices, which may also need location permissions.")
-                    Spacer(modifier = Modifier.size(16.dp))
-                    Button(
-                        onClick = {
-                            permissionsNeeded.launchMultiplePermissionRequest()
-                        }
-                    ) {
-                        Text(text = "Request permissions")
-                    }
-                }
+        Button(
+            onClick = {
+                permissionsNeeded.launchMultiplePermissionRequest()
             }
-
-            ProcessStateConstants.PERMISSIONS_GRANTED -> {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "All permissions OK.")
-                    Spacer(modifier = Modifier.size(16.dp))
-                    Button(
-                        onClick = {
-                            sendToNearbyDeviceViewModel.transitionToNewState(ProcessStateConstants.DISCOVERY_STARTED)
-                        }
-                    ) {
-                        Text(text = "Start discovery")
-                    }
-                }
-            }
-
-            ProcessStateConstants.PERMISSIONS_DENIED -> {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "Without the necessary permissions, importing from nearby devices is not possible.")
-                    Spacer(modifier = Modifier.size(16.dp))
-                    Button(
-                        onClick = {
-                            permissionsNeeded.launchMultiplePermissionRequest()
-                        }
-                    ) {
-                        Text(text = "Request permissions")
-                    }
-                }
-            }
-
-            ProcessStateConstants.DISCOVERY_STARTED -> {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "All permissions OK, looking for devices...")
-                }
-            }
-
-            ProcessStateConstants.FOUND_PARTNER -> {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "Found a partner!")
-                }
-            }
-            ProcessStateConstants.AUTHENTICATION_OK -> TODO()
-            ProcessStateConstants.AUTHENTICATION_DENIED -> TODO()
-            ProcessStateConstants.CONNECTION_ESTABLISHED -> TODO()
-            ProcessStateConstants.CONNECTION_DENIED -> TODO()
-            ProcessStateConstants.SENDING -> TODO()
-            ProcessStateConstants.DISCONNECTED -> TODO()
-            ProcessStateConstants.DONE -> {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "All done.")
-                }
-            }
-            ProcessStateConstants.CANCELLED -> {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "Cancelled.")
-                }
-            }
-            ProcessStateConstants.ERROR -> {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "Some error occurred. It may help to move away from this screen and try it all again.")
-                }
-            }
-
+        ) {
+            Text(text = "Request permissions")
         }
     }
+}
+
+@Composable
+private fun ProcessStatePermissionsGrantedScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "All permissions OK.")
+        Text(text = "Now starting discovery...")
+    }
+}
+
+@Composable
+private fun ProcessStateDiscoveryStartedScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "All permissions OK, looking for devices...")
+    }
+}
+
+@Composable
+private fun ProcessStatePartnerFoundScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Found a partner!")
+    }
+}
+
+@Composable
+private fun ProcessStateConnectingScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Connecting...")
+    }
+}
+
+@Composable
+private fun ProcessStateDoneScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "All done.")
+    }
+}
+
+@Composable
+private fun ProcessStateCancelledScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Cancelled.")
+    }
+}
+
+@Composable
+private fun ProcessStateErrorScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Some error occurred. It may help to move away from this screen and try it all again.")
+    }
+}
+
+@Composable
+fun SendToNearbyBottomBar(
+    processState: ProcessState,
+    transition: (ProcessStateConstants, String) -> Unit
+) {
+    BottomAppBar(
+        actions = {
+            IconButton(onClick = {
+                transition(ProcessStateConstants.CANCELLED, "Cancelled")
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "Cancel"
+                )
+            }
+        },
+        floatingActionButton = {
+            when (processState.currentState) {
+                ProcessStateConstants.AWAITING_PERMISSIONS -> {
+                    ExtendedFloatingActionButton(
+                        text = { Text(text = "Request Permissions") },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.Check,
+                                contentDescription = "Request permissions"
+                            )
+                        },
+                        onClick = {
+                            // how do we do this?
+                        },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+                    )
+                }
+                ProcessStateConstants.PERMISSIONS_GRANTED -> {
+                    ExtendedFloatingActionButton(
+                        text = { Text(text = "Discover Devices") },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                                contentDescription = "Discover Devices"
+                            )
+                        },
+                        onClick = {
+                            transition(ProcessStateConstants.DISCOVERY_STARTED, "Discovering")
+                        },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+                    )
+                }
+                ProcessStateConstants.AUTHENTICATION_OK -> {
+                    ExtendedFloatingActionButton(
+                        text = { Text(text = "Send") },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.Check,
+                                contentDescription = "Send Processes"
+                            )
+                        },
+                        onClick = {
+                            transition(ProcessStateConstants.SENDING, "Sending")
+                        },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+                    )
+                }
+                else -> {
+                    ExtendedFloatingActionButton(
+                        text = { Text(text = "Cancel") },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.Clear,
+                                contentDescription = "Cancel"
+                            )
+                        },
+                        onClick = {
+                            transition(ProcessStateConstants.CANCELLED, "Cancel")
+                        },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+                    )
+                }
+            }
+        }
+    )
 }
