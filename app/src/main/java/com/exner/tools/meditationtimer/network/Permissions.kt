b32ca6b01503a7e94eb1f3e12399055c03dd1d53
jexner <jan.exner@gmx.net>
2024-07-29 11:16:33 +0200

    in which our hero turns to something else for a moment

---
+++ app/src/main/java/com/exner/tools/meditationtimer/network/Permissions.kt
@@ -0,0 +1,77 @@
+package com.exner.tools.meditationtimer.network
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+
+class Permissions(
+    val context: Context
+) {
+
+    private val currentReleaseLevel: Int = Build.VERSION.SDK_INT
+
+    val allNecessaryPermissions = when (currentReleaseLevel) {
+
+        Build.VERSION_CODES.TIRAMISU, Build.VERSION_CODES.UPSIDE_DOWN_CAKE -> {
+            listOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_ADVERTISE,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.NEARBY_WIFI_DEVICES,
+            )
+        }
+
+        Build.VERSION_CODES.S_V2, Build.VERSION_CODES.S -> {
+            listOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_ADVERTISE,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        }
+
+        Build.VERSION_CODES.R, Build.VERSION_CODES.Q -> {
+            listOf(
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        }
+
+        else -> {
+            listOf(
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+        }
+    }
+
+    private val missingPermissions: MutableList<String> = mutableListOf()
+
+    // check all the permissions we need
+    init {
+        allNecessaryPermissions.forEach { permission ->
+            val checkResult =
+                ContextCompat.checkSelfPermission(context, permission)
+            if (checkResult == PackageManager.PERMISSION_DENIED) {
+                missingPermissions.add(permission)
+            } else if (checkResult == PackageManager.PERMISSION_GRANTED) {
+                missingPermissions.remove(permission)
+            }
+        }
+    }
+}
