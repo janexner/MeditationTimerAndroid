a68665193321390de156fd5b665ac3d0361b1227
jexner <jan.exner@gmx.net>
2024-10-27 22:13:55 +0100

    in which our hero uses AssistedInject

--- app/src/main/java/com/exner/tools/meditationtimer/ui/ProcessDeleteViewModel.kt
+++ app/src/main/java/com/exner/tools/meditationtimer/ui/ProcessDeleteViewModel.kt
@@ -7,14 +7,17 @@ import androidx.lifecycle.viewModelScope
 import com.exner.tools.meditationtimer.data.persistence.MeditationTimerChainingDependencies
 import com.exner.tools.meditationtimer.data.persistence.MeditationTimerDataIdAndName
 import com.exner.tools.meditationtimer.data.persistence.MeditationTimerDataRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-@HiltViewModel
-class ProcessDeleteViewModel @Inject constructor(
+@HiltViewModel(assistedFactory = ProcessDeleteViewModel.ProcessDeleteViewModelFactory::class)
+class ProcessDeleteViewModel @AssistedInject constructor(
+    @Assisted val uuid: String,
     private val repository: MeditationTimerDataRepository
-): ViewModel() {
+) : ViewModel() {
 
     private val _processName: MutableLiveData<String> = MutableLiveData("")
     val processName: LiveData<String> = _processName
@@ -22,31 +25,36 @@ class ProcessDeleteViewModel @Inject constructor(
     private val _processIsTarget: MutableLiveData<Boolean> = MutableLiveData(false)
     val processIsTarget: LiveData<Boolean> = _processIsTarget
 
-    private val _processChainingDependencies: MutableLiveData<MeditationTimerChainingDependencies> = MutableLiveData(null)
-    val processChainingDependencies: LiveData<MeditationTimerChainingDependencies> = _processChainingDependencies
+    private val _processChainingDependencies: MutableLiveData<MeditationTimerChainingDependencies> =
+        MutableLiveData(null)
+    val processChainingDependencies: LiveData<MeditationTimerChainingDependencies> =
+        _processChainingDependencies
 
-    fun checkProcess(processUuid: String?) {
-        if (processUuid != null) {
-            viewModelScope.launch {
-                val process = repository.loadProcessByUuid(processUuid)
-                if (process != null) {
-                    _processName.value = process.name
-                    val newDependentProcessUuids = repository.getUuidsOfDependentProcesses(process)
-                    val newDependentProcesses = mutableListOf<MeditationTimerDataIdAndName>()
-                    newDependentProcessUuids.forEach {
-                        val tmpProcess = repository.loadProcessByUuid(it)
-                        if (tmpProcess != null) {
-                            newDependentProcesses.add(MeditationTimerDataIdAndName(it, tmpProcess.name))
-                        }
+    init {
+        viewModelScope.launch {
+            val process = repository.loadProcessByUuid(uuid)
+            if (process != null) {
+                _processName.value = process.name
+                val newDependentProcessUuids = repository.getUuidsOfDependentProcesses(process)
+                val newDependentProcesses = mutableListOf<MeditationTimerDataIdAndName>()
+                newDependentProcessUuids.forEach {
+                    val tmpProcess = repository.loadProcessByUuid(it)
+                    if (tmpProcess != null) {
+                        newDependentProcesses.add(MeditationTimerDataIdAndName(it, tmpProcess.name))
                     }
-                    val chainingDependencies = MeditationTimerChainingDependencies(
-                        newDependentProcesses
-                    )
-                    _processChainingDependencies.value = chainingDependencies
-                    _processIsTarget.value = true
                 }
+                val chainingDependencies = MeditationTimerChainingDependencies(
+                    newDependentProcesses
+                )
+                _processChainingDependencies.value = chainingDependencies
+                _processIsTarget.value = true
             }
         }
+    }
+
+    @AssistedFactory
+    interface ProcessDeleteViewModelFactory {
+        fun create(uuid: String): ProcessDeleteViewModel
     }
 
     fun deleteProcess(processUuid: String) {
